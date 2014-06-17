2e7730f32791253166b5fc0a55f370e7ce694474
Bart <bart@devlap>
2014-06-17 21:54:56 +1200

    Initial communication with cheap transmitter and receiver.

---
+++ Car/Car.ino
@@ -0,0 +1,43 @@
+#include <RHReliableDatagram.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+const int ledPin = 13;
+bool ledOn = false;
+
+// Singleton instance of the radio driver
+RH_ASK driver;
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  if (!manager.init())
+    Serial.println("init failed");
+}
+
+uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+
+void loop()
+{
+  if (manager.available())
+  {
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfrom(buf, &len, &from))
+    {
+      digitalWrite(ledPin, ledOn ? HIGH : LOW);
+      ledOn = !ledOn;
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+    }
+  }
+}
+
---
+++ Controller/Controller.ino
@@ -0,0 +1,43 @@
+// ask_reliable_datagram_client.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple addressed, reliable messaging client
+// with the RHReliableDatagram class, using the RH_ASK driver to control a ASK radio.
+// It is designed to work with the other example ask_reliable_datagram_server
+// Tested on Arduino Mega, Duemilanova, Uno, Due, Teensy
+
+#include <RHReliableDatagram.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+
+// Singleton instance of the radio driver
+RH_ASK driver;
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+void setup() 
+{
+  Serial.begin(9600);
+  if (!manager.init())
+    Serial.println("init failed");
+}
+
+uint8_t data[] = "Hello World!";
+// Dont put this on the stack:
+uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+
+void loop()
+{
+  Serial.println("Sending to ask_reliable_datagram_server");
+    
+  // Send a message to manager_server
+  if (!manager.sendto(data, sizeof(data), SERVER_ADDRESS))
+  {
+  }
+    Serial.println("sendtoWait failed");
+  delay(500);
+}
+
